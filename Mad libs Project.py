22a4073a5c94843d6bdc0c4e89beb999908231d2
jheniferhalm <jenhalm83@gmail.com>
2025-02-25 05:41:47 +1000

    Mad libs Project.py

--- Mad libs Project.py
+++ Mad libs Project.py
@@ -14,5 +14,3 @@ print()
 print("Your story is: ")
 
 print(f'The other day, I was really in trouble. It all started when I saw a very\n{adjective.lower()} {animal.lower()} {verb.lower()} down the hallway. "{exclamation.capitalize()}!" I yelled. But all \nI could think to do was to {verb.lower()} over and over. Miraculously,\nthat caused it to stop, but not before it tried to\n{verb} right in front of my family. ')
-#NOT FINISHED
-#ADD STUFF FOR EXTRA POINTS
